dfde21a16dd3df273af5a96382ca6d3a94f88ab2
Uggah <luccagreschner@gmail.com>
2023-04-16 21:10:08 +0200

    Add ReturnError method

--- internal/http_server/http_error.go
+++ internal/http_server/http_error.go
@@ -1,6 +1,10 @@
 package http_server
 
-import "time"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
 
 type Error struct {
 	Timestamp time.Time `json:"timestamp"`
@@ -13,5 +17,23 @@ func NewHTTPError(message string, path string) Error {
 		time.Now(),
 		message,
 		path,
+	}
+}
+
+func ReturnError(w http.ResponseWriter, r *http.Request, status int, reason string) {
+	w.WriteHeader(status)
+
+	httpError := NewHTTPError(reason, r.RequestURI)
+
+	bytes, err := json.Marshal(httpError)
+	if err != nil {
+		logger.Error("Couldn't encode http error!")
+		return
+	}
+
+	_, err = w.Write(bytes)
+	if err != nil {
+		logger.Error("Couldn't write http error!")
+		return
 	}
 }
