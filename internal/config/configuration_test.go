d40e1d0e39285f90d961d641f653e47f0e95dfe8
Lucca Greschner <luccagreschner@gmail.com>
2023-05-05 11:59:41 +0200

    Add test for env configuration

--- internal/config/configuration_test.go
+++ internal/config/configuration_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/knadh/koanf/providers/confmap"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
@@ -67,4 +68,46 @@ func TestCheckConfig(t *testing.T) {
 	err = checkConfig()
 	assert.ErrorIs(t, err, ErrInvalidConfigParameter)
 	assert.Equal(t, "invalid config parameter: refresh token secret is not set", err.Error())
+}
+
+func TestInitConfigENV(t *testing.T) {
+	t.Setenv("EXCUBITOR_LOGGING_LOG-LEVEL", "TRACE")
+	t.Setenv("EXCUBITOR_HTTP_AUTH_JWT_ACCESS-TOKEN-SECRET", "abcde")
+	t.Setenv("EXCUBITOR_HTTP_AUTH_JWT_REFRESH-TOKEN-SECRET", "abcde")
+
+	movedConfig := false
+
+	if _, err := os.Stat("config.yml"); !os.IsNotExist(err) {
+		if err := os.Rename("config.yml", "config.yml.bak"); err != nil {
+			t.Error(err)
+			return
+		}
+		movedConfig = true
+	}
+
+	_, err := os.Create("config.yml")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer func() {
+		if err := os.Remove("config.yml"); err != nil {
+			t.Error(err)
+		}
+
+		if movedConfig {
+			if err := os.Rename("config.yml.bak", "config.yml"); err != nil {
+				t.Error(err)
+				return
+			}
+		}
+	}()
+
+	if err := InitConfig(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	assert.Equal(t, "TRACE", k.String("logging.log_level"))
 }
