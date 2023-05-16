3fa70a58ea1ae8731e29ab18d3353c28ef3ddc73
Lucca Greschner <luccagreschner@gmail.com>
2023-05-16 19:04:51 +0200

    Fix unset maxDensity in a HIST request not being handled properly

--- internal/http_server/websocket/websocket.go
+++ internal/http_server/websocket/websocket.go
@@ -158,7 +158,7 @@ func handleHIST(conn net.Conn, content *Message) error {
 	params := &HistoryRequestParameters{
 		From:       time.Time{},
 		Until:      time.Now(),
-		MaxDensity: "1ns",
+		MaxDensity: "",
 	}
 
 	if content.Value != "" {
@@ -173,30 +173,48 @@ func handleHIST(conn net.Conn, content *Message) error {
 		}
 	}
 
-	logger.Info(fmt.Sprintf("HistoryRequestParameters{ From: %s, Until: %s, MaxDensity: %s }", params.From, params.Until, params.MaxDensity))
+	logger.Trace(fmt.Sprintf("Parsed HistoryRequestParameters { From: %s, Until: %s, MaxDensity: %s } from %s", params.From, params.Until, params.MaxDensity, clientAddress))
 
-	var maxDensity time.Duration
-	switch duration := params.MaxDensity.(type) {
-	case float64:
-		maxDensity = time.Duration(duration)
-	case string:
+	var history db.History
+	reader := db.GetReader()
+
+	if params.MaxDensity != "" {
+		var maxDensity time.Duration
+		switch duration := params.MaxDensity.(type) {
+		case float64:
+			maxDensity = time.Duration(duration)
+		case string:
+			var err error
+			maxDensity, err = time.ParseDuration(duration)
+			if err != nil {
+				return err
+			}
+		}
+
 		var err error
-		maxDensity, err = time.ParseDuration(duration)
+		history, err = reader.GetHistoryEntriesFromUntilThinned(string(content.Target), params.From, params.Until, maxDensity)
 		if err != nil {
+			logger.Error(fmt.Sprintf("Error when retrieving history data of target %s: %s", content.Target, err.Error()))
+
+			if err := sendMessage(conn, NewMessage(ERR, content.Target, "Internal server error!")); err != nil {
+				return fmt.Errorf("%w: %s", FatalWebsocketError, err)
+			}
+
 			return err
 		}
-	}
 
-	reader := db.GetReader()
-	history, err := reader.GetHistoryEntriesFromUntilThinned(string(content.Target), params.From, params.Until, maxDensity)
-	if err != nil {
-		logger.Error(fmt.Sprintf("Error when retrieving history data of target %s: %s", content.Target, err.Error()))
+	} else {
+		var err error
+		history, err = reader.GetHistoryEntriesFromUntil(string(content.Target), params.From, params.Until)
+		if err != nil {
+			logger.Error(fmt.Sprintf("Error when retrieving history data of target %s: %s", content.Target, err.Error()))
 
-		if err := sendMessage(conn, NewMessage(ERR, content.Target, "Internal server error!")); err != nil {
-			return fmt.Errorf("%w: %s", FatalWebsocketError, err)
+			if err := sendMessage(conn, NewMessage(ERR, content.Target, "Internal server error!")); err != nil {
+				return fmt.Errorf("%w: %s", FatalWebsocketError, err)
+			}
+
+			return err
 		}
-
-		return err
 	}
 
 	historyJson, err := json.Marshal(history)
